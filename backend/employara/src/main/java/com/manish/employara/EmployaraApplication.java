bbb75fc16fb0666ec58893e2978330fdc3a512ca
manish-kumar0 <singhmanish847377@gmail.com>
2025-06-25 18:46:57 +0530

    Initial commit

---
+++ backend/employara/src/main/java/com/manish/employara/EmployaraApplication.java
@@ -0,0 +1,25 @@
+package com.manish.employara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployaraApplication.class, args);
+	}
+
+}
+
+// package com.manish.employara;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class EmployaraApplication {
+//     public static void main(String[] args) {
+//         SpringApplication.run(EmployaraApplication.class, args);
+//     }
+// }
